291d3c4d5d01c36a716416296bebbdc91cfd3873
Pavel Vekshyn <pvekshyn@deloitte.com>
2023-02-07 15:47:12 +0300

    role outbox job add kv

--- Outbox.Job/src/Outbox.Job.Host/Program.cs
+++ Outbox.Job/src/Outbox.Job.Host/Program.cs
@@ -1,6 +1,22 @@
+using Azure.Identity;
 using Outbox.Job.Infrastructure.Extensions;
 
-IHost host = Host.CreateDefaultBuilder(args)
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureAppConfiguration((context, config) =>
+{
+    var settings = config.Build();
+    var keyVaultName = settings.GetSection("KeyVaultName")?.Value;
+    if (!string.IsNullOrEmpty(keyVaultName))
+    {
+        var keyVaultEndpoint = $"https://{keyVaultName}.vault.azure.net";
+        config.AddAzureKeyVault(
+            new Uri(keyVaultEndpoint),
+            new DefaultAzureCredential());
+    }
+});
+
+var host = builder
     .ConfigureServices((context, services) =>
     {
         services.AddQuartzJob(context.Configuration)
