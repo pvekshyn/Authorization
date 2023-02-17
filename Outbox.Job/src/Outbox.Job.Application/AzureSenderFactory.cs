b83ea1f151ae0988f22556b59fd33939e6378641
Pavel Vekshyn <pvekshyn@deloitte.com>
2023-02-17 19:08:40 +0300

    outbox sender factory

---
+++ Outbox.Job/src/Outbox.Job.Application/AzureSenderFactory.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Outbox.Job.Infrastructure
+{
+    internal class AzureSenderFactory
+    {
+        private Dictionary<string, ServiceBusSender> _senders = new Dictionary<string, ServiceBusSender>();
+
+        public ServiceBusSender GetSender(string eventName, ServiceBusClient client)
+        {
+            if (_senders.TryGetValue(eventName, out var sender))
+                return sender;
+
+            sender = client.CreateSender(eventName);
+            _senders.Add(eventName, sender);
+            return sender;
+        }
+    }
+}
--- Outbox.Job/src/Outbox.Job.Application/OutboxPublisherAzure.cs
+++ Outbox.Job/src/Outbox.Job.Application/OutboxPublisherAzure.cs
@@ -1,23 +1,24 @@
 ﻿using Azure.Identity;
 using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json.Linq;
 using Outbox.Job.Infrastructure.Models;
 
 namespace Outbox.Job.Infrastructure;
 internal class OutboxPublisherAzure : IOutboxPublisher
 {
     private readonly IOutboxRepository _outboxRepository;
-    private readonly ServiceBusClient client;
-    private readonly ServiceBusSender sender;
+    private readonly ServiceBusClient _client;
+    private readonly AzureSenderFactory _senderFactory;
 
     public OutboxPublisherAzure(IOutboxRepository outboxRepository)
     {
         _outboxRepository = outboxRepository;
 
-        client = new ServiceBusClient(
+        _client = new ServiceBusClient(
             "pv-authorization.servicebus.windows.net",
             new DefaultAzureCredential());
 
-        sender = client.CreateSender("role.sdk.events.rolecreatedevent");
+        _senderFactory = new AzureSenderFactory(_client);
     }
 
     public async Task PublishAsync(OutboxMessage outboxMessage)
@@ -28,7 +29,11 @@ internal class OutboxPublisherAzure : IOutboxPublisher
 
             var message = new ServiceBusMessage(outboxMessage.Message);
 
-            await sender.SendMessageAsync(message);
+            JObject json = JObject.Parse(outboxMessage.Message);
+            var type = json["$type"].ToString();
+            var eventName = type.Split(",")[0];
+
+            await _senderFactory.GetSender(eventName).SendMessageAsync(message);
 
             _outboxRepository.Delete(outboxMessage.Id);
         }
